a6b3007ffd63921a02fdd0b21f67b9e23c6acf35
NguyenDoQuangMinh2112 <nguyendoquangminh222@gmail.com>
2024-11-14 13:35:01 +0700

    cmt

--- src/models/quizs/quiz_questions.model.ts
+++ src/models/quizs/quiz_questions.model.ts
@@ -4,13 +4,11 @@ import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 const QUIZ_QUESTION_COLLECTION_NAME = 'quizQuestion'
 
 const QUIZ_QUESTION_COLLECTION_SCHEMA = Joi.object({
-  quiz: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
+  quizId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
   question: Joi.string().required().trim().strict(),
   options: Joi.array().optional(),
   correct_answer: Joi.string().optional(),
-  question_type: Joi.string().optional().valid('multiple_choice', 'true_false', 'short_answer', 'essay'),
-  difficulty: Joi.number().optional().integer().min(1).max(5),
-  topic: Joi.string().optional().trim().strict(),
+  explain: Joi.string().optional(),
   createdAt: Joi.date().timestamp('javascript').default(Date.now),
   updatedAt: Joi.date().timestamp('javascript').default(null),
   _destroy: Joi.boolean().default(false)
